3aca91fa8554dc749d47df5197e7dc5ece563ae4
Vittorio Vitale <vvitale@sissa.it>
2022-02-26 18:08:05 +0100

    added shadows

--- shadows.jl
+++ shadows.jl
@@ -5,6 +5,68 @@ include("mps.jl")
 include("mpo.jl")
 
 
+function save_rhoi(A::MPS)
+    right_normalize!(A)
+    p=zeros(A.N)
+    @tensor temp[:]:=A.data[1][1,-1,2]*conj(A.data[1][1,-2,2])
+    p[1]=real(temp[2,2])
+    for i in 1:A.N
+        sA = size(A.data[i])
+        U,S,V = svd(reshape(A.data[i],(sA[1]*sA[2],sA[3])),full=false)
+        V=V'  
+        A.data[i] = reshape( U,( sA[1], sA[2], :)) 
+        if i<A.N
+            S=diagm(S)
+            @tensor A.data[i+1][:] := S[-1,1 ] * V[ 1,2 ] * A.data[i+1][2,-2,-3] 
+            @tensor temp[:]:=A.data[i+1][1,-1,2]*conj(A.data[i+1][1,-2,2])
+            p[i+1]=real(temp[2,2])
+        else
+            @tensor temp[:]:=A.data[i][1,-1,2]*conj(A.data[i][1,-2,2])
+            p[i]=real(temp[2,2])
+        end
+        
+    end   
+    return p
+end
+
+
+function sample_shadows(A::MPS,nu::Int,nm::Int,lA::Int)
+    shadows=Dict()
+    ket=[[1 ; 0],[0 ; 1]]
+    for r in 1:nu
+        WlA=MPO()
+        Initialize!("Local_Haar",WlA,lA)
+            
+        if lA<A.N
+            W=MPO()
+            Initialize!("Id",W,A.N)
+            for i in 1:lA
+                W.data[i]=Base.copy(WlA.data[i])
+            end
+        else
+            W=copy(WlA)
+        end 
+             
+        state=W*A
+        p_state=save_rhoi(state)[1:lA]
+        for m in 1:nm        
+            shadows[r,m]=MPO()
+            Initialize!("Zeros",shadows[r,m],2,1,lA)
+            k=zeros(Int,lA)
+            p_extract=rand(lA)
+            k[p_extract.>p_state].=1
+            for (qubit,spin) in enumerate(k)
+                shadows[r,m].data[qubit][1,1,:,:]=3*kron(ket[spin+1],ket[spin+1]')-1.0*I(2) 
+            end
+            shadows[r,m]=(WlA*shadows[r,m])*adjoint(WlA)
+        end
+    end
+    return shadows
+end
+
+
+
+
 function calculate_shadows_MPO(nu::Int,nm::Int,lU::Array,lstr::Array,qubit_set::Array)
     ket=[[1 ; 0],[0 ; 1]]
     chi=1
@@ -39,10 +101,14 @@ function calc_dist(rhos::Dict)
     
     for i in 1:length(rhos)
         for j in i+1:length(rhos)
-            println(typeof(rhos[i]-rhos[j])*(rhos[i]-rhos[j]))
-            distrhos[i,j]=real(tr((rhos[i]-rhos[j])*(rhos[i]-rhos[j]))^(1/2))
+            distrhos[i,j]=real(tr(rhos[i,1]*adjoint(rhos[i,1]))-
+                tr(rhos[i,1]*adjoint(rhos[j,1]))-
+                tr(rhos[j,1]*adjoint(rhos[i,1]))+
+                tr(rhos[j,1]*adjoint(rhos[j,1])))^(1/2)
         end
-        distrhos=distrhos+distrhos'
+    end
+    distrhos=distrhos+distrhos'
+    for i in 1:length(rhos)
         idx[i,:]=sortperm(distrhos[i,:])
         distrhos[i,:]=distrhos[i,idx[i,:]]
     end
