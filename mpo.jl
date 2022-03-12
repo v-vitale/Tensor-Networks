275f26a0a0b907b3bedbea394189458de83fad35
Vittorio Vitale <vvitale@sissa.it>
2022-03-12 12:12:08 +0100

    open system resolved

--- mpo.jl
+++ mpo.jl
@@ -359,45 +359,6 @@ function Initialize!(s::String,W::MPO,alpha::Float64,J::Float64,hz::Float64,k::I
     end
 end
 
-function Initialize!(s::String,W::MPO,J::Array,N::Int)
-    chi=5
-    k=3
-    hz=0.
-    if s=="Brydges"
-        c = 0.001*J[:,1]
-        λ = J[:,2]
-        
-        σx = [0 1; 1 0]
-        σz = [1 0; 0 -1]
-        Id2= [1 0; 0 1]
-                
-        Wt = im *  zeros(chi,chi,d,d)
-        Wt1 = im *  zeros(1,chi,d,d)
-        Wt2 = im *  zeros(chi,1,d,d)
-        
-        Wt[1, 1, :, :] = Id2
-        for i in 1:k
-            Wt[1+i, 1, :, :] = σx
-            Wt[1+i,1+i,:, :] = exp(-λ[i])*Id2
-            Wt[end, 1+i,:, :] = -exp(-λ[i])*c[i]*σx
-        end
-        Wt[end, 1, :, :] = -hz*σz
-        Wt[end,end,:,:] = Id2
-        Wt1  = reshape(Wt[end,:,:,:],(1,chi,2,2))
-        Wt2 = reshape(Wt[:,1,:,:],(chi,1,2,2))
-    
-        W.N=N
-        W.data[1] = Base.copy(Wt1)
-        for i in 2:(N-1)
-            W.data[i] = Base.copy(Wt)
-        end
-        W.data[N] = Base.copy(Wt2)
-        return "Brydges MPO"  
-    else
-        @warn "Wrong parameters"
-    end
-end
-
 
 function Initialize!(s::String,W::MPO,alpha::Float64,J::Float64,hz::Float64,γp::Float64,γm::Float64,γz::Float64,k::Int,N::Int)
     chi=2*k+2
@@ -467,8 +428,9 @@ function Initialize!(s::String,W::MPO,alpha::Float64,J::Float64,hz::Float64,γp:
     end
 end
 
-function Initialize!(s::String,W::MPO,J::Array,N::Int) 
-    if s=="Brydges2"
+function Initialize_Brydges!(s::String,W::MPO,N::Int) 
+    J=[223.394 0.778458; 292.381 0.539682; 208.695 0.182969]
+    if s=="Closed"
         d=2
         k=3
         chi=2*k+2
@@ -507,7 +469,7 @@ function Initialize!(s::String,W::MPO,J::Array,N::Int)
         end
         W.data[N] = Base.copy(Wt2)
         return "Brydges MPO"  
-    elseif s=="OpenBrydges2"
+    elseif s=="Open"
         k=3
         chi=4*k+2
         hz=0.
@@ -520,7 +482,7 @@ function Initialize!(s::String,W::MPO,J::Array,N::Int)
         σp = [0 1; 0 0]
         σm = [0 0; 1 0]
         σx = [0 1; 1 0]
-        σz = [1 0; 0 1]
+        σz = [1 0; 0 -1]
         Id2= [1 0; 0 1]
 
         Id4= kron(Id2,Id2)
@@ -530,8 +492,7 @@ function Initialize!(s::String,W::MPO,J::Array,N::Int)
         σm2 = kron(Id2,σm)
         σz1 = kron(σz,Id2)
         σz2 = kron(Id2,σz)
-        σx4 = kron(σx,σx)
-        Diss= γm*kron(σm,σm)-0.5*γm*kron(σp*σm,Id2)-0.5*γm*kron(Id2,σp*σm)+γx*σx4-γx*Id4
+        Diss= γm*kron(σm,σm)-0.5*γm*kron(σp*σm,Id2)-0.5*γm*kron(Id2,σp*σm)+γx*kron(σx,σx)-γx*Id4
         Wt = im *  zeros(chi,chi,d,d)
         Wt1 = im *  zeros(1,chi,d,d)
         Wt2 = im *  zeros(chi,1,d,d)
@@ -554,7 +515,7 @@ function Initialize!(s::String,W::MPO,J::Array,N::Int)
             Wt[3*k+1+i,3*k+1+i,:, :] = exp(-λ[i])*Id4
             Wt[end, 3*k+1+i,:, :] = -exp(-λ[i])*c[i]*σp2
         end
-        Wt[end, 1, :, :] = -im*Diss
+        Wt[end, 1, :, :] = im*Diss
         Wt[end,end,:,:] = Id4
         Wt1  = reshape(Wt[end,:,:,:],(1,chi,d,d))
         Wt2 = reshape(Wt[:,1,:,:],(chi,1,d,d))
