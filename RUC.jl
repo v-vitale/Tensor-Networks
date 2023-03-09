8ed5a6d1b0ad83e0736b18b4c27c876d7d9f724f
Vittorio Vitale <52443808+v-vitale@users.noreply.github.com>
2023-03-09 14:19:18 +0100

    RUC update

--- RUC.jl
+++ RUC.jl
@@ -1,7 +1,7 @@
 include("mps.jl")
 include("mpo.jl")
 
-function RUC_layer(s::String,N::Int)  
+function RUC_layer(N::Int)  
   function CUE(nh)
       U = (randn(nh,nh)+im*randn(nh,nh))/sqrt(2)
       q,r = qr(U)
@@ -10,16 +10,7 @@ function RUC_layer(s::String,N::Int)
       U = ph.*q
       return U
   end
-
-  
-  if mod(N,2) != 0
-    @warn "Odd number of sites"
-  end
-  if s=="odd"
-        return [reshape(CUE(4),(2,2,2,2)) for i in 1:(Int(N/2)-1)]
-  elseif s=="even"
-        return [reshape(CUE(4),(2,2,2,2)) for i in 1:Int(N/2)]
-  end
+  return [reshape(CUE(4),(2,2,2,2)) for i in 1:Int(N/2)]
 end 
 
       
