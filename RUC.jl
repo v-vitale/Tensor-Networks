31201c0cf21a3c2ed5e23781fe92e38b905bf0a7
Vittorio Vitale <52443808+v-vitale@users.noreply.github.com>
2023-03-09 11:54:14 +0100

    CUE

--- RUC.jl
+++ RUC.jl
@@ -2,13 +2,22 @@ include("mps.jl")
 include("mpo.jl")
 
 function RUC_layer(s::String,N::Int)  
+  function CUE(nh)
+      U = (randn(nh,nh)+im*randn(nh,nh))/sqrt(2)
+      q,r = qr(U)
+      d = r[diagind(r)]
+      ph = d./abs.(d)
+      U = ph.*q
+      return U
+  end
+
+  
   if mod(N,2) != 0
     @warn "Odd number of sites"
   end
-  dist = Haar(4)
   if s=="odd"
-        return [reshape(rand(dist,d),(2,2,2,2)) for i in 1:(Int(N/2)-1)]
+        return [reshape(CUE(4),(2,2,2,2)) for i in 1:(Int(N/2)-1)]
   elseif s=="even"
-        return [reshape(rand(dist,d),(2,2,2,2)) for i in 1:Int(N/2)]
+        return [reshape(CUE(4),(2,2,2,2)) for i in 1:Int(N/2)]
   end
 end 
