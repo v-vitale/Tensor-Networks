181fcbf8b4afab547c43bd980d9f9a0be8da1340
Vittorio Vitale <52443808+v-vitale@users.noreply.github.com>
2023-03-09 11:14:01 +0100

    RUC added

---
+++ RUC.jl
@@ -0,0 +1,14 @@
+include("mps.jl")
+include("mpo.jl")
+
+function RUC_layer(s::String,W::MPO,N::Int)  
+  if mod(N,2) \= 0
+    @warn "Odd number of sites"
+  end
+  dist = Haar(4)
+  if s=="odd"
+        return [reshape(rand(dist,d),(2,2,2,2)) for i in 1:(Int(N/2)-1)]
+  elseif s=="even"
+        return [reshape(rand(dist,d),(2,2,2,2)) for i in 1:Int(N/2)]
+  end
+end 
