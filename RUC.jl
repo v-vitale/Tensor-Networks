e6706c401a01c044b474879093defe8361c5461b
Vittorio Vitale <vvitale@sissa.it>
2023-03-10 12:28:22 +0100

    RUC measure

--- RUC.jl
+++ RUC.jl
@@ -13,4 +13,21 @@ function RUC_layer(N::Int)
   return [reshape(CUE(4),(2,2,2,2)) for i in 1:N-1]
 end 
 
-      
+function RUC_measure!(ψt::MPS,p::Float64)
+    for site in 1:ψt.N
+        if rand() < p
+            move_orthogonality_center!(ψt,site)
+            @tensor temp[:]:=ψt.data[site][1,-1,2]*conj(ψt.data[site][1,-2,2])
+            rho=temp/tr(temp)
+            pup=real(rho[1,1])
+            P=MPO()
+            if rand() < pup
+                Initialize!("proj",P,[0],[site],ψt.N)
+            else
+                Initialize!("proj",P,[1],[site],ψt.N)
+            end
+            ψt=P*ψt
+            right_normalize!(ψt)
+        end
+    end
+end
